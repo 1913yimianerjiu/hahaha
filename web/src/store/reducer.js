b1d721fbe86f3321a1ed1abdd2baf67308423733
guomeng922 <52997866+guomeng922@users.noreply.github.com>
2019-10-26 09:48:27 +0800

    aa

--- web/src/store/reducer.js
+++ web/src/store/reducer.js
@@ -13,18 +13,4 @@ export default (preState=State,action) => {
   }
   return newData
 }
-// import State from './state'
-// export default (preState=State,action)=>{
-//   let newData = JSON.parse(JSON.stringify(preState))
-//   let {type,params}=action
-//   switch (type) {
-//     case 'CHANGE_TOKEN_MODAL':
-//         newData.tokenModal=params
-//       break;
-  
-//     default:
-//       break;
-//   }
-//   return newData
-// }
 
--- web/src/store/store.js
+++ web/src/store/store.js
@@ -3,13 +3,3 @@ import thunk from 'redux-thunk'
 import reducer from './reducer'
 export default createStore(reducer,applyMiddleware(thunk))
 
-// import {createStore,applyMiddleware} from 'redux'
-// // applyMiddleware 中间件
-// import thunk from 'redux-thunk'
-
-// import reducer from  './reducer'
-// console.log(thunk)
-// export default createStore(reducer,applyMiddleware(thunk))
-
-
-
